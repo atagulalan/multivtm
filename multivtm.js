c7864fd5028c379bcaeddbd7ec9fd2633060bb11
Ata Gülalan <xava@hotmail.com.tr>
2020-04-13 15:21:12 +0300

    Initial

---
+++ multivtm.js
@@ -0,0 +1,111 @@
+#!/usr/bin/env node
+
+const path = require('path')
+const fs = require('fs')
+const { exec } = require('child_process')
+let encoderApp = './EncoderApp.exe'
+let outputDir = 'instances'
+let configsDir = 'configs'
+let outputFile = 'out.txt'
+let args = process.argv.slice(2)
+
+function help() {
+  console.log(
+    [
+      '',
+      'Usage: multivtm [options]',
+      '       multivtm -e ./encoder.exe',
+      '',
+      'Options:',
+      '       -e, --encoder <file>      EncoderApp.exe path. Can be relative.',
+      '       -i, --instance <folder>   Instances will be outputted this folder.',
+      '       -c, --configs <folder>    Configs folder for each instance to work.',
+      '       -o, --output <file>       Outputs returned lines to specified file',
+      '',
+    ].join('\n')
+  )
+}
+
+if (args.includes('--encoder') || args.includes('-e')) {
+  argIndex = args.findIndex((e) => e === '--encoder' || '-e')
+  encoderApp = args.splice(argIndex, 2)[1]
+}
+if (args.includes('--instance') || args.includes('-i')) {
+  argIndex = args.findIndex((e) => e === '--instance' || '-i')
+  outputDir = args.splice(argIndex, 2)[1]
+}
+if (args.includes('--configs') || args.includes('-c')) {
+  argIndex = args.findIndex((e) => e === '--configs' || '-c')
+  configsDir = args.splice(argIndex, 2)[1]
+}
+if (args.includes('--output') || args.includes('-o')) {
+  argIndex = args.findIndex((e) => e === '--output' || '-o')
+  outputFile = args.splice(argIndex, 2)[1]
+}
+
+var deleteFolderRecursive = function (path) {
+  if (fs.existsSync(path)) {
+    fs.readdirSync(path).forEach(function (file, index) {
+      var curPath = path + '/' + file
+      if (fs.lstatSync(curPath).isDirectory()) {
+        deleteFolderRecursive(curPath)
+      } else {
+        fs.unlinkSync(curPath)
+      }
+    })
+    fs.rmdirSync(path)
+  }
+}
+
+function run(options, configs) {
+  let reConfig = []
+  configs.forEach((el) => {
+    reConfig.push('-c')
+    reConfig.push(el)
+  })
+  fs.copyFileSync(
+    path.join(`${encoderApp}`),
+    path.join(__dirname, `${outputDir}/${options.configPath}/EncoderApp.exe`)
+  )
+  exec(
+    `"${path.join(
+      __dirname,
+      `${outputDir}/${options.configPath}/EncoderApp.exe`
+    )}" ${[...reConfig].join(' ')} > ${outputFile} 2>&1`,
+    {
+      cwd: path.join(__dirname, `${outputDir}/${options.configPath}/`),
+      windowsHide: true,
+    },
+    function (err, data) {
+      process.exit(1)
+    }
+  )
+}
+
+if (args[0] === '--help' || args[0] === '--h' || args[0] === 'help') {
+  help()
+} else {
+  deleteFolderRecursive(path.join(__dirname, `${outputDir}`))
+  fs.mkdirSync(path.join(__dirname, `${outputDir}/`))
+  fs.readdir(path.join(__dirname, `${configsDir}/`), function (err, files) {
+    if (err) return console.log('Unable to scan directory: ' + err)
+    files.forEach((configPath) => {
+      fs.mkdirSync(path.join(__dirname, `${outputDir}/${configPath}/`))
+      fs.readdir(
+        path.join(__dirname, `${configsDir}/${configPath}/`),
+        function (err, files) {
+          if (err) return console.log('Unable to scan directory: ' + err)
+          let list = []
+          files.forEach(function (file) {
+            fs.copyFileSync(
+              path.join(__dirname, `${configsDir}/${configPath}/${file}`),
+              path.join(__dirname, `${outputDir}/${configPath}/${file}`)
+            )
+            if (file.endsWith('.cfg')) list.push(file)
+          })
+          run({ configPath }, list)
+        }
+      )
+    })
+  })
+}
